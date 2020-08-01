bd89eb483754ae70ef901847042bc82c4b0ad926
Himanshu Patel <hpatel27299@gmail.com>
2020-08-02 02:15:51 +0530

    Created basic layour of course project

--- Recipe-Book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe-Book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Dosa (with Sambhar)', 'A South Indian dish', 'https://www.cookwithmanali.com/wp-content/uploads/2020/05/Masala-Dosa-500x500.jpg'),
+    new Recipe('Hyderabadi Chicken Biryani', 'A variety of biryani from Hyderabad ', 'https://pachakam.com/wp-content/uploads/2019/07/Hyderabadi-Chicken-Biryani-Pachakam-2676.jpg')
+  ];
 
   constructor() { }
 
--- Recipe-Book/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-Book/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Rice', 1),
+    new Ingredient('Chicken', 1)
+  ];
 
   constructor() { }
 
