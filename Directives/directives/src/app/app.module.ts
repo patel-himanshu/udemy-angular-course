9e5af14d42ab8198fbc09580b70db2d6954b6dd1
Himanshu Patel <hpatel27299@gmail.com>
2020-08-09 23:54:09 +0530

    Added Basic Highligh Directive

---
+++ Directives/directives/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
