2ab957d778df110b5aff2c840101752a6f21c779
Himanshu Patel <hpatel27299@gmail.com>
2020-09-12 00:10:53 +0530

    Replaced EventEmitter with Subject in Recipe Book

--- Recipe-Book/src/app/recipes/recipes.component.ts
+++ Recipe-Book/src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service'; 
 
 @Component({
@@ -9,14 +8,7 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe_r: Recipe;
+  constructor() {}
 
-  constructor(private recipeService: RecipeService) { }
-
-  ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => { 
-        this.selectedRecipe_r = recipe;
-    });
-  }
+  ngOnInit() {}
 }
